d3965ad79284ed46408bb1f9eaffe418ebf70e90
vidtv <nikit1533@yandex.ru>
2018-03-05 02:44:14 +0300

    Part of methods of first test case was written

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,2 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainClass {
+
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Никита\\IdeaProjects\\test_market_yandex\\drivers\\geckodriver.exe");
+
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+
+        // Задаем неявное ожидание 3 секунды для поиска товаров
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        driver.get("https://yandex.ru");
+
+        MarketPage marketPage = new MarketPage(driver);
+
+        marketPage.getTVsList();
+    }
 }
--- src/main/java/MarketPage.java
+++ src/main/java/MarketPage.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class MarketPage {
@@ -8,16 +9,74 @@ public class MarketPage {
         this.driver = driver;
     }
 
+    int minPrice = 20000;
+
     private By yandexMarketButton = By.xpath("//a[@data-id='market']");
-    private By electronicaButton = By.xpath("//a[contains(text(),'Электроника')]");
-    private By televisors = By.xpath("//a[contains(text(), 'Телевизоры') and @class='link topmenu__subitem']");
-    private By extendedSearch = By.xpath("//a[@class='link i-bem link_js_inited' and contains(text(),'Перейти ко всем фильтрам')]");
-    private By minPrice = By.xpath("//input[@id='glf-pricefrom-var']");
+    private By electronicaButton = By.xpath("//li[@data-department='Электроника']");
+    private By electronicaButtonOther = By.xpath("//div[@class='n-navigation-horizontal__inner']/div[2]/a[text()='Электроника']");
+    private By televisors = By.xpath("//a[text()='Аудио- и видеотехника']/following-sibling::div/a[text()='Телевизоры']");
+    private By minPriceInputField = By.xpath("//input[@id='glf-pricefrom-var']");
     private By chooseSamsung = By.xpath("//label[contains(text(),'Samsung')]/../..");
     private By chooseLG = By.xpath("//label[contains(text(),'LG')]/../..");
+    private By applyButton = By.xpath("//span[@class='button__text'][contains(text(),'Применить')]/..");
     private By itemCard = By.xpath("//div[contains(@class, 'n-snippet-list')]/div[contains(@class, 'n-snippet-card2')]");
     private By itemLink = By.xpath("//div[contains(@class, 'n-snippet-list')]/div[contains(@class, 'n-snippet-card2')][1]//div[@class='n-snippet-card2__title']/a");
     private By itemSearch = By.xpath("//input[@id='header-search']");
     private By suggestItem = By.xpath("//div[@class='suggest2__group'][1]//span[@class='suggest2-rich-item__text']/b");
+
+    /* метод возвращает true или false в зависимости от отрисовки списка категорий Маркета*/
+    public static boolean isElectronicaButtonExists(By electronica) {
+        try {
+             driver.findElement(electronica);
+             return true;
+            }
+            catch (NoSuchElementException a) {
+                return false;
+            }
+    }
+
+    /* метод возвращает ссылку на пункт "Электроника" в зависимости от отрисовки списка категорий Маркета*/
+    public By electronicaButtonFind() {
+        if (isElectronicaButtonExists(electronicaButton)) {
+            return electronicaButton;
+        }
+        else return electronicaButtonOther;
+    }
+
+    public void yandexMarketButtonClick() {
+        driver.findElement(yandexMarketButton).click();
+    }
+
+    public void electronicaChoose() {
+        driver.findElement(this.electronicaButtonFind()).click();
+    }
+
+    public void televisorsChoose() {
+        driver.findElement(televisors).click();
+    }
+
+    public void minPriceInput() {
+        driver.findElement(minPriceInputField).sendKeys(String.valueOf(minPrice));
+    }
+
+    public void brandChoose() {
+        driver.findElement(chooseSamsung).click();
+        driver.findElement(chooseLG).click();
+    }
+
+    public void toApply() {
+        driver.findElement(applyButton).click();
+    }
+
+    public void getTVsList() {
+        this.yandexMarketButtonClick();
+        this.electronicaChoose();
+        this.televisorsChoose();
+        this.minPriceInput();
+        this.brandChoose();
+        this.toApply();
+    }
+
+
 
 }
