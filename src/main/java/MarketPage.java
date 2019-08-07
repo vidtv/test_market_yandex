f3cb266ded70e2a5809127d7ddad3107c24b35a2
borisovno <nikit1534@gmail.com>
2019-08-08 01:27:36 +0300

    Refactoring with Selenide

--- src/main/java/MarketPage.java
+++ src/main/java/MarketPage.java
@@ -1,112 +1,75 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
-import java.util.List;
+import static com.codeborne.selenide.Selenide.*;
 
 public class MarketPage {
-    public static WebDriver driver;
 
-    public MarketPage (WebDriver driver) {
-        this.driver = driver;
-    }
-
-    int minPrice = 20000;
-
-    private By yandexMarketButton = By.xpath("//a[@data-id='market']");
-    private By electronicaButton = By.xpath("//li[@data-department='Электроника']");
-    private By electronicaButtonOther = By.xpath("//div[@class='n-navigation-horizontal__inner']/div[2]/a[text()='Электроника']");
-    private By televisors = By.xpath("//a[text()='Аудио- и видеотехника']/following-sibling::div/a[text()='Телевизоры']");
-    private By minPriceInputField = By.xpath("//input[@name='Цена от']");
-    private By chooseSamsung = By.xpath("//span[contains(text(),'Samsung')]");
-    private By chooseLG = By.xpath("//span[contains(text(),'LG')]");
-    private By itemSearch = By.xpath("//input[@id='header-search']");
-    private By suggestItem = By.xpath("//div[@class='suggest2__group'][1]//span[@class='suggest2-rich-item__text']/b");
-
-    /* метод возвращает true или false в зависимости от отрисовки списка категорий Маркета */
-    public static boolean isElectronicaButtonExists(By electronica) {
-        try {
-             driver.findElement(electronica);
-             return true;
-            }
-            catch (NoSuchElementException a) {
-                return false;
-            }
-    }
-
-    /* метод возвращает ссылку на пункт "Электроника" в зависимости от отрисовки списка категорий Маркета */
-    public By electronicaButtonFind() {
-        if (isElectronicaButtonExists(electronicaButton)) {
-            return electronicaButton;
-        }
-        else return electronicaButtonOther;
-    }
-
-    public void yandexMarketButtonClick() {
-        driver.findElement(yandexMarketButton).click();
-    }
-
-    public void electronicaChoose() {
-        driver.findElement(this.electronicaButtonFind()).click();
-    }
-
-    public void televisorsChoose() {
-        driver.findElement(televisors).click();
-    }
-
-    public void minPriceInput() {
-        driver.findElement(minPriceInputField).sendKeys(String.valueOf(minPrice));
-    }
-
-    public void brandChoose() {
-        driver.findElement(chooseSamsung).click();
-        driver.findElement(chooseLG).click();
-    }
-
-    /* массив карточек товаров */
-    private List<WebElement> itemsList;
-
-    public void setItemsList() {
-        itemsList = driver.findElements(By.xpath("//div[@class='n-snippet-card2__title']/a"));
+    public SelenideElement getBrandChoose(String brandName) {
+        return $(By.xpath("//span[contains(text(), \'" + brandName + "\')]"));
     }
 
     public void getTVsList() {
-        this.yandexMarketButtonClick();
-        this.electronicaChoose();
-        this.televisorsChoose();
-        this.minPriceInput();
-        this.brandChoose();
-        this.setItemsList();
+        getYandexMarketButton().click();
+        getElectronicaButton().click();
+        getTelevisors().click();
+        int MIN_PRICE = 20000;
+        getMinPriceInputField().sendKeys(String.valueOf(MIN_PRICE));
+        getBrandChoose("Samsung").click();
+        getBrandChoose("LG").click();
+        getItemsList();
+    }
+
+    /* ввод названия первого товара в строку поиска */
+    public void searchItem(String item) {
+        getItemSearch().sendKeys(item);
+    }
+
+    public SelenideElement getYandexMarketButton() {
+        return $(By.xpath("//a[@data-id='market']"));
+    }
+
+    public SelenideElement getElectronicaButton() {
+        return $$(".n-w-tab__control-caption").findBy(Condition.text("Электроника"));
+    }
+
+    public SelenideElement getTelevisors() {
+        return $(By.xpath("//a[text()='Телевизоры']"));
+    }
+
+    public SelenideElement getMinPriceInputField() {
+        return $("#glpricefrom");
+    }
+
+    /* массив карточек товаров */
+
+    public SelenideElement getItemSearch() {
+        return $(By.xpath("//input[@id='header-search']"));
+    }
+
+    public SelenideElement getSuggestItem() {
+        return $(By.xpath("//div[@class='suggest2__group'][1]//span[@class='suggest2-rich-item__text']/b"));
+    }
+
+    public ElementsCollection getItemsList() {
+        return $$(By.xpath("//div[@class='n-snippet-card2__title']/a"));
     }
 
     /* подсчет кол-ва карточек товаров на странице */
     public int getNumOfItems() {
-        return itemsList.size();
+        return getItemsList().size();
     }
 
     /* название марки товара в первой карточке */
     public String getNameOfFirstItem() {
-        WebElement firstItem = itemsList.get(0);
-        String itemTitle = firstItem.getAttribute("title");
-        return itemTitle;
-    }
-
-    /* ввод названия первого товара в строку поиска */
-    public void searchFirstItem() {
-        String firstItemName = this.getNameOfFirstItem();
-        driver.findElement(itemSearch).sendKeys(firstItemName);
+        SelenideElement firstItem = getItemsList().get(0);
+        return firstItem.getAttribute("title").substring("ЖК-телевизор ".length());
     }
 
     /* извлечение названия предложенного товара */
     public String getSuggestedItem() {
-        WebElement suggestedItem = driver.findElement(suggestItem);
-        String suggestedItemName = suggestedItem.getText();
-        return suggestedItemName;
-    }
-
-    public void getMarketPage() {
-        this.getTVsList();
-        this.searchFirstItem();
+        return getSuggestItem().getText();
     }
 }
--- src/test/java/TestPage.java
+++ src/test/java/TestPage.java
@@ -1,38 +1,35 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.concurrent.TimeUnit;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestPage {
 
-    private WebDriver driver;
     private MarketPage marketPage;
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Никита\\IdeaProjects\\test_market_yandex\\drivers\\geckodriver.exe");
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get("https://yandex.ru");
-        marketPage = new MarketPage(driver);
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nikita\\Documents\\test_market_yandex\\drivers\\chromedriver.exe");
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+        open("https://yandex.ru");
+        marketPage = new MarketPage();
     }
 
     @Test
     public void numOfItemsCheck() {
         marketPage.getTVsList();
         int numOfItems = marketPage.getNumOfItems();
-        int numOfItemsExpected = 12;
-        Assert.assertEquals(numOfItems, numOfItemsExpected);
+        int NUM_OF_ITEMS_EXPECTED = 48;
+        Assert.assertEquals(numOfItems, NUM_OF_ITEMS_EXPECTED);
     }
 
     @Test
     public void firstItemCheck() {
-        marketPage.getMarketPage();
-        String firstItemName = marketPage.getNameOfFirstItem();
+        marketPage.getTVsList();
+        marketPage.searchItem(marketPage.getNameOfFirstItem());
+        String firstItemName = marketPage.getNameOfFirstItem().toLowerCase();
         String firstSuggestedName = marketPage.getSuggestedItem();
-        String firstItemNameLow = firstItemName.toLowerCase();
-        Assert.assertEquals(firstItemNameLow, firstSuggestedName);
+        Assert.assertEquals(firstItemName, firstSuggestedName);
     }
 }
